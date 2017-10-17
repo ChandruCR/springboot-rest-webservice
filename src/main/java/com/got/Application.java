f12721ef515f851ea60d0fbd64fc2dfd01273826
Chandru <mailchandrucr@gmail.com>
2017-10-17 22:51:12 +0200

    Enabling Cross Origin Requests

--- src/main/java/com/got/Application.java
+++ src/main/java/com/got/Application.java
@@ -2,6 +2,10 @@ package com.got;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +14,17 @@ public class Application {
 		// TODO Auto-generated method stub
 		SpringApplication.run(Application.class, args);
 	}
+	
+	
+	//Below code will enable Cross Origin Requests
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/got").allowedOrigins("http://localhost:7777");
+            }
+        };
+    }
 
 }
